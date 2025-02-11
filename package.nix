8e69f5e0b991520f81df51d499809e1a711eaec8
rocketcamel <luca_lise@icloud.com>
2025-02-11 00:14:39 +0000

    chore: bump version

--- package.nix
+++ package.nix
@@ -6,7 +6,7 @@
 }:
 
 let
-  rversion = "0.6.0-rc.5";
+  rversion = "0.6.0-rc.6";
   registryVersion = "registry.0.2.0-rc.2";
 in
 stdenv.mkDerivation rec {
@@ -19,7 +19,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://github.com/pesde-pkg/pesde/releases/download/${version}/${pname}-${rversion}-linux-x86_64.zip";
-    sha256 = "sha256-aAPUoZLyvPLmng8GfSfXbMmZdlJPjkqNXbFZWwtGvhU=";
+    sha256 = "sha256-gov6+t7shBiIR97r8Kmwq8Yxf5IShJ0XijIajyWhenE=";
   };
 
   unpackPhase = "unzip $src";
